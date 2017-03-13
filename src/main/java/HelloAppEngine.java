bd610856418b3ec6e9c48e268b6657d9897f4959
zumatec <robin-delaporte@hotmail.fr>
2017-03-13 12:49:47 +0100

    Récupération des twit

--- src/main/java/HelloAppEngine.java
+++ src/main/java/HelloAppEngine.java
@@ -10,19 +10,27 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class HelloAppEngine extends HttpServlet {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
-	  Twitter twitter = TwitterFactory.getSingleton();
-	twitter.setOAuthConsumer("Lqm5FAXfFRCvDEGm3UeaIwpfs", "iSbYBRRvqfuLXmSPpLLr999dxugyeLinjhoQag8ZjPKyGhp6Le");
-	Query query = new Query("source:twitter4j yusukey");
+	  ConfigurationBuilder cb = new ConfigurationBuilder();
+	  cb.setDebugEnabled(true)
+	    .setOAuthConsumerKey("Lqm5FAXfFRCvDEGm3UeaIwpfs")
+	    .setOAuthConsumerSecret("iSbYBRRvqfuLXmSPpLLr999dxugyeLinjhoQag8ZjPKyGhp6Le")
+	    .setOAuthAccessToken("2447499750-vgTS2BReU4WThv4gDGbFacsyMDs9qvB76OS6y1F")
+	    .setOAuthAccessTokenSecret("W09ihvscegGTcFKrSBOHU9lqq0jGSHKqJuxib7ZJgVdP2");
+	  TwitterFactory tf = new TwitterFactory(cb.build());
+	  Twitter twitter = tf.getInstance();
+	//twitter.setOAuthConsumer("Lqm5FAXfFRCvDEGm3UeaIwpfs", "iSbYBRRvqfuLXmSPpLLr999dxugyeLinjhoQag8ZjPKyGhp6Le");
+	Query query = new Query("from:realDonaldTrump");
     QueryResult result;
 	try {
 		result = twitter.search(query);
 	    for (Status status : result.getTweets()) {
-	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+	    	response.getWriter().print("@" + status.getUser().getScreenName() + ":" + status.getText());
 	    }
 	} catch (TwitterException e) {
 		// TODO Auto-generated catch block
